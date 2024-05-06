81857ab2165f29247c89ca8ce3e1eb0a8907be39
AnthonyBerisha <berishaanthony@gmail.com>
2024-05-06 14:52:16 +0200

    rendering

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
 import Grid from '@/components/Grid.vue';
 
 const items = [
-  {key: '1', name: 'item 1'},
-  {key: '2', name: 'item 2'},
-  {key: '3', name: 'item 3'},
-  {key: '4', name: 'item 4'},
-  {key: '5', name: 'item 5'},
+  {name: 'item 1'},
+  {name: 'item 2'},
+  {name: 'item 3'},
+  {name: 'item 4'},
+  {name: 'item 5'},
 ];
 
 </script>
