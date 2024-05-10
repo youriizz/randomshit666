82c161c699a1b3e5c4c61572d1e8d6a4f29ab508
AnthonyBerisha <berishaanthony@gmail.com>
2024-05-10 17:43:53 +0200

    Early thumbnails config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Grid from '@/components/Grid.vue';
 import { GridItem } from '@/components/GridItem.vue';
+import NetflixThumbnail from '@/components/Netflix/NetflixThumbnail.vue';
 import TrafficLight from "@/components/TrafficLight.vue";
 
 const items: GridItem[] = [
@@ -9,7 +10,7 @@ const items: GridItem[] = [
   { name: 'item 3', route: "" },
   { name: 'item 4', route: "" },
   { name: 'item 5', route: "" },
-  { name: 'Netflix', route: "netflix" },
+  { name: 'Netflix', route: "netflix", component: NetflixThumbnail },
 ];
 
 </script>
