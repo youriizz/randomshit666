5f78f33010feac5bece72c0c5927198036181d9f
AnthonyBerisha <berishaanthony@gmail.com>
2024-06-18 02:52:30 +0200

    feat: select different models

--- src/components/HailMary/HailMary.vue
+++ src/components/HailMary/HailMary.vue
@@ -1,14 +1,22 @@
 <template>
-    <Renderer ref="renderer" antialias :orbit-ctrl="{ enableDamping: true }" resize>
-        <Camera :position="{ x: 1, y: 0, z: 0 }" />
-        <Scene>
-            <AmbientLight></AmbientLight>
-            <GltfModel src="/src/assets/HailMary/Hail Mary.glb" />
-        </Scene>
-    </Renderer>
+    <div>
+        <RadialMenu @select:model="selectModel" :models="models" />
+        <Renderer ref="renderer" antialias :orbit-ctrl="{ enableDamping: true }" resize="window">
+            <Camera :position="{ x: 1, y: 0, z: 0 }" />
+            <Scene background="white">
+                <AmbientLight></AmbientLight>
+                <GltfModel v-if="models[0].active" src="/models/hailmary.glb" @error="handleError" @load="onLoad" />
+                <GltfModel v-if="models[1].active" src="/models/korean.glb" @error="handleError" @load="onLoad" />
+                <GltfModel v-if="models[2].active" src="/models/meatboy.glb" @error="handleError" @load="onLoad" />
+                <GltfModel v-if="models[3].active" src="/models/forklift.glb" @error="handleError" @load="onLoad" />
+            </Scene>
+        </Renderer>
+    </div>
 </template>
 
-<script>
+<script setup>
+import RadialMenu from "@/components/HailMary/RadialMenu.vue";
+import { ref } from 'vue';
 import {
     AmbientLight,
     Camera,
@@ -17,13 +25,42 @@ import {
     Scene,
 } from 'troisjs';
 
-export default {
-    components: {
-        AmbientLight,
-        Camera,
-        GltfModel,
-        Renderer,
-        Scene,
+const models = ref([
+    {
+        path: "/models/Hail Mary.glb",
+        active: true,
+        icon: "+"
     },
-};
+    {
+        path: "/models/korean.glb",
+        active: false,
+        icon: "+"
+    },
+    {
+        path: "/models/Hail Mary.glb",
+        active: false,
+        icon: "+"
+    },
+    {
+        path: "/models/Hail Mary.glb",
+        active: false,
+        icon: "+"
+    },
+]);
+
+function selectModel(index) {
+    models.value.forEach((model) => {
+        model.active = false;
+    });
+
+    models.value[index].active = true;
+}
+
+function handleError(error) {
+    console.log(error);
+}
+
+function onLoad(model) {
+    console.log(model)
+}
 </script>
---
+++ src/components/HailMary/RadialMenu.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="radial-menu">
+        <div @click="select(index)" class="button" v-for="(model, index) in models" :key="index"
+            :class="{ active: model.active }">
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps(['models']);
+
+const emit = defineEmits(['select:model']);
+
+function select(index) {
+    emit('select:model', index)
+}
+
+</script>
+
+<style scoped>
+.radial-menu {
+    position: absolute;
+    top: 5%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 200px;
+    height: 50;
+    border: 1px solid black;
+}
+
+.button {
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    opacity: 50%;
+    border: 1px solid black;
+}
+
+.button.active {
+    background-color: red;
+    opacity: 100%;
+}
+
+.button:hover {
+    scale: 1.1;
+    animation: ease-out 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940);
+}
+
+.slide-in-left {
+    -webkit-animation: slide-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    animation: slide-in-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+
+@-webkit-keyframes slide-in-left {
+    0% {
+        -webkit-transform: translateX(-1000px);
+        transform: translateX(-1000px);
+        opacity: 0;
+    }
+
+    100% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0);
+        opacity: 1;
+    }
+}
+
+@keyframes slide-in-left {
+    0% {
+        -webkit-transform: translateX(-1000px);
+        transform: translateX(-1000px);
+        opacity: 0;
+    }
+
+    100% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0);
+        opacity: 1;
+    }
+}
+
+
+@media only screen and (min-width: 1200px) {
+    .radial-menu {
+        width: 50px;
+        height: 200px;
+        top: 50%;
+        left: 5%;
+        flex-direction: column;
+    }
+}
+</style>
