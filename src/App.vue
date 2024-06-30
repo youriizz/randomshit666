c75de34997dbdd8542f2f4ad2e60f55e093e1570
AnthonyBerisha <berishaanthony@gmail.com>
2024-06-30 16:18:58 +0200

    prevent grabbing things

--- src/App.vue
+++ src/App.vue
@@ -15,4 +15,16 @@ body,
   font-family: "Helvetica neue", sans-serif;
   font-weight: 500;
 }
+
+* {
+  -webkit-touch-callout: none;
+  /* Disable callout, tap highlight, etc. */
+
+  -webkit-user-select: none;
+  /* Disable user select */
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
 </style>
