dca888c71d05a9808245b4d98b87e09977badd73
AnthonyBerisha <berishaanthony@gmail.com>
2024-07-01 01:09:16 +0200

    fix conflicts

--- src/components/Instagram/Instagram.vue
+++ src/components/Instagram/Instagram.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <!-- <Post v-for="post in posts" :key="post.id" :post="post" /> -->
+        <Post v-for="post in posts" :key="post.id" :post="post" />
     </div>
 </template>
 
